39ffdd7376c354a081dbedfa4b87f1a57834eaa1
rferolla <raissa.ferolla@gmail.com>
2022-10-27 01:49:11 -0300

    initial

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HelloWorld\n");
+            Console.Write("Meu nome é Raissa");
+            Console.ReadLine();
+           
+
+
+        }
+    }
+}
