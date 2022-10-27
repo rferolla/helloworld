4992c6f02e2124a32d16f3dad9a70c74384d68fd
rferolla <raissa.ferolla@gmail.com>
2022-10-27 02:07:26 -0300

    organize files

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HelloWorld");
+            Console.ReadLine();
+        }
+    }
+}
