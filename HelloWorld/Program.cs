4958a8c38d90351b0c212f1716c624ebee14bf69
rferolla <raissa.ferolla@gmail.com>
2022-10-28 02:41:03 -0300

    TiposEVariaveisExercise

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,28 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWorld");
+            // Int - 156 como 12334
+            // Float - 15.6 como 232.132
+            // Bool - true or false
+            // String - "Hello World"
+            // Char - 'a'
+
+            int segundaGuerraMundial = 1942;
+            string myFavoriteColor = "pink";
+            float carSpeed = 200.5f;
+            bool worldWarII = true;
+
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(carSpeed);
+            Console.WriteLine(worldWarII);  
+            Console.WriteLine(myFavoriteColor);
+
+            carSpeed = 201.2f;
+            myFavoriteColor = "purple";
+
+            Console.WriteLine(carSpeed);
+            Console.WriteLine(myFavoriteColor); 
+
             Console.ReadLine();
         }
     }
